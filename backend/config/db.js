6396a686116d91679bbe384cd3aab56cdbfeddaa
Niraj Kumar <neerajkumar89212@gmail.com>
2026-04-21 01:39:28 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    console.log("🔄 Connecting to MongoDB...");
+
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
